57b99a2b1d8e1d0f5e613a060d8f727d118684d4
Thryck <jens.heindryckx@gmail.com>
2014-09-12 14:12:44 +0200

    een alert was nog niet aangepast, localhost aangepast naar smartschool in agenda.js

--- smartschool/js/agenda.js
+++ smartschool/js/agenda.js
@@ -9,14 +9,14 @@ $(document).ready(function() {
 			editable: true,
                         selectable: true,
                         selectHelper: true,
-                        events: "http://localhost/Kalender/events.php",
+                        events: "http://localhost/smartschool/smartschool/events.php",
                         select: function(start, end, allDay) {
                          var title = prompt('Event Title:');
                          if (title) {
                          start = $.fullCalendar.formatDate(start, "yyyy-MM-dd HH:mm:ss");
                          end = $.fullCalendar.formatDate(end, "yyyy-MM-dd HH:mm:ss");
                          $.ajax({
-                         url: 'http://localhost/Kalender/add_events.php',
+                         url: 'http://localhost/smartschool/smartschool/add_events.php',
                          data: 'title='+ title+'&start='+ start +'&end='+ end ,
                          type: "POST",
                          success: function(json) {
@@ -40,7 +40,7 @@ $(document).ready(function() {
                          start = $.fullCalendar.formatDate(event.start, "yyyy-MM-dd HH:mm:ss");
                          end = $.fullCalendar.formatDate(event.end, "yyyy-MM-dd HH:mm:ss");
                          $.ajax({
-                         url: 'http://localhost/Kalender/update_events.php',
+                         url: 'http://localhost/smartschool/smartschool/update_events.php',
                          data: 'title='+ event.title+'&start='+ start +'&end='+ end +'&id='+ event.id ,
                          type: "POST",
                          success: function(json) {
@@ -52,11 +52,11 @@ $(document).ready(function() {
                          start = $.fullCalendar.formatDate(event.start, "yyyy-MM-dd HH:mm:ss");
                          end = $.fullCalendar.formatDate(event.end, "yyyy-MM-dd HH:mm:ss");
                          $.ajax({
-                         url: 'http://localhost/Kalender/update_events.php',
+                         url: 'http://localhost/smartschool/smartschool/update_events.php',
                          data: 'title='+ event.title+'&start='+ start +'&end='+ end +'&id='+ event.id ,
                          type: "POST",
                          success: function(json) {
-                         alert("OK");
+                         alert("Update geslaagd");
                          }
                          });
 
@@ -65,7 +65,7 @@ $(document).ready(function() {
 
                                 if (confirm("Wil je dit event echt verwijderen?")) {
                                         $.ajax({
-                                        url: 'http://localhost/Kalender/delete_events.php',
+                                        url: 'http://localhost/smartschool/smartschool/delete_events.php',
                                         data: 'id=' + event.id,
                                         type: "POST",
                                         success: function(json){
